d0bd5672a1b92f4a2d352aaa06b43c9a8a5d9226
Johann Paez <johann.paez@mail.escuelaing.edu.co>
2020-01-22 09:56:39 -0500

    ClassWork Part II

---
+++ src/main/java/edu/eci/arsw/patterns/App.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
